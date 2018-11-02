2a94f4d108c8a2f8ec1dde671b42bb293da8c931
anthonydevelops <anthonydevelops@gmail.com>
2018-11-02 02:34:26 -0700

    Initial Major.js file

---
+++ client/src/components/Major.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+
+/* 
+ * This component displays the major requirement page for 
+ * Computer Science B.S. . Eventually, we will create a process to 
+ * generate layouts that work for all majors, but until we figure 
+ * that out, a single major page will work.
+ */
+
+export default class Major extends Component {
+  // Initial Database -> Page setup -- Anthony
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // Any state variables we need
+    };
+  }
+
+  // Lower Division Function -- Hannah
+
+  // Upper Division Function -- Anthony
+
+  // Electives -- Hannah
+
+  // DC -- Anthony
+
+  // Capstone -- Hannah
+}
